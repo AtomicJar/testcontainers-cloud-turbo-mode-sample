10ee1dae9d14138bd611918a1dcc0c829fa7ab04
ignasi <ignasi.lopez.luna@gmail.com>
2022-10-06 09:25:01 +0200

    Removed redis related tests, instead run a Kafka container and verify that has been initialized

--- src/test/java/org/testcontainers/cloud/turbomode/sample/TestcontainersCloud3Test.java
+++ src/test/java/org/testcontainers/cloud/turbomode/sample/TestcontainersCloud3Test.java
@@ -1,46 +1,20 @@
 package org.testcontainers.cloud.turbomode.sample;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
-import redis.clients.jedis.Jedis;
 
-import java.util.Optional;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-/**
- * Integration test for Redis-backed cache implementation.
- */
 @Testcontainers
 public class TestcontainersCloud3Test {
 
     @Container
-    public GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis:3.0.6"))
-            .withExposedPorts(6379);
-    private Cache cache;
-
-    @BeforeEach
-    public void setUp() {
-        Jedis jedis = new Jedis(redis.getHost(), redis.getMappedPort(6379));
-        cache = new RedisBackedCache(jedis, "test");
-    }
+    private KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:6.2.1"));
 
     @Test
     public void testFindingAnInsertedValue() {
-        cache.put("foo", "FOO");
-        Optional<String> foundObject = cache.get("foo", String.class);
-
-        assertThat(foundObject.isPresent()).as("When an object in the cache is retrieved, it can be found").isTrue();
-        assertThat(foundObject.get()).as("When we put a String in to the cache and retrieve it, the value is the same").isEqualTo("FOO");
-    }
-
-    @Test
-    public void testNotFindingAValueThatWasNotInserted() throws InterruptedException {
-        Optional<String> foundObject = cache.get("bar", String.class);
-        assertThat(foundObject.isPresent()).as("When an object that's not in the cache is retrieved, nothing is found").isFalse();
+        Assertions.assertNotNull(kafka.getBootstrapServers());
     }
 }
