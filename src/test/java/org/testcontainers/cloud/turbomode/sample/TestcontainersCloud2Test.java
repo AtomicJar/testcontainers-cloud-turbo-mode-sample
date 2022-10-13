97e144debd313770def7cf71d18f6f99437f89ee
ignasi <ignasi.lopez.luna@gmail.com>
2022-10-13 14:39:37 +0200

    No need to use @Testcontainers so org.testcontainers:junit-jupiter not needed

--- src/test/java/org/testcontainers/cloud/turbomode/sample/TestcontainersCloud2Test.java
+++ src/test/java/org/testcontainers/cloud/turbomode/sample/TestcontainersCloud2Test.java
@@ -1,9 +1,7 @@
 package org.testcontainers.cloud.turbomode.sample;
 
 import org.junit.jupiter.api.Test;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
 public class TestcontainersCloud2Test extends AbstractIntegrationTest {
 
     @Test
